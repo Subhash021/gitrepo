a14dcec690dea32b0678ab70c096296de41ee74a
Subhash021 <142431615+Subhash021@users.noreply.github.com>
2023-10-10 16:55:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucketoo99"
+  bucket = "devopsbysubhashterraserverbucketoo99"
 }
 
 resource "aws_iam_user" "seven" {
